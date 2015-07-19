5cc709b46adecfbe503c4407f9e4601c59da3c6a
Denis Gukov <denguk@gmail.com>
2015-07-20 00:43:19 +0500

    Auth by phone number

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,24 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :confirmable
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable,
+         authentication_keys: [:login]
+
+  def login=(login)
+    @login = login
+  end
+
+  def login
+    @login || self.phone_number || self.email
+  end
+
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions.to_h).where(["lower(phone_number) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    else
+      where(conditions.to_h).first
+    end
+  end
+
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,7 +14,7 @@ Rails.application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
@@ -38,4 +38,27 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  config.action_mailer.delivery_method :smtp
+  config.action_mailer.smtp_settings = {
+    #enable_starttls_auto: true,
+    openssl_verify_mode: 'none',
+    tls: false,
+    enable_starttls_auto: true,
+    address: 'smtp.gmail.com',
+    domain: 'gmail.com',
+    port: 587,
+    authentication: 'login',
+    user_name: "denguk@gmail.com",
+    password: 'NochkaHamik'
+
+
+    # address: 'smtp.mail.ru',
+    # tls: true,
+    # port: 25,
+    # authentication: :plain,
+    # user_name: 'denguk@mail.ru',
+    # password: 'Mama280361'
+  }
+
 end
